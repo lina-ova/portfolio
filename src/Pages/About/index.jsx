d836e01fb41c55b1fd7003fca86ac52918183d3c
Polina <nazpolina00@gmail.com>
2024-10-31 12:14:20 +0200

    update about me

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -3,49 +3,49 @@ import myImg from '../../img/m1.gif'
 import cat from '../../img/cat.jpg'
 import './index.css'
 
-const Header =()=>{
+const Header = () => {
     const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn())
     return (
         <>
-        <ScrollPage page={3}>
-            <Animator animation={ZoomInScrollOut}>
-                <div className="section-title about-me">
-                    About Me
-                </div>
-            </Animator>
-        </ScrollPage>
-        <ScrollPage page={4}>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(-1000, 500))}>
-                <div className="background-image-container-cat">
-                    <img src={cat} alt='cat' />  
+            <ScrollPage page={3}>
+                <Animator animation={ZoomInScrollOut}>
+                    <div className="section-title about-me">
+                        About Me
+                    </div>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={4}>
+                <Animator animation={batch(Fade(), Sticky(), MoveOut(-1000, 500))}>
+                    <div className="background-image-container-cat">
+                        <img src={cat} alt='cat' />
 
-                </div>
-            </Animator>
-            <Animator animation={batch(Fade(), Sticky(), MoveIn(-1000, 500))}>
-                <div className="background-image-container-girl">
-                    <img src={myImg} alt='my picture' />  
-                    <div className="black-background1"></div>
-                </div>
-            </Animator>
+                    </div>
+                </Animator>
+                <Animator animation={batch(Fade(), Sticky(), MoveIn(-1000, 500))}>
+                    <div className="background-image-container-girl">
+                        <img src={myImg} alt='my picture' />
+                        <div className="black-background1"></div>
+                    </div>
+                </Animator>
 
-            <Animator animation={batch(Fade(), Sticky(), MoveOut(1000, 500))}>
-            
-                <div className="details">
-                    <h2 className="gretting">
-                        Hi there!
-                    </h2>
-                    <p className="bio">
-                    I am a developer based in Helsinki, Finland. I am currently pursuing a bachelor's degree in computer science at the University of Helsinki. This is my second year of studies.
-                    </p>
-                    <p className="bio">
-                    My endless energy fuels my pursuit of many interests, hobbies, and fields of study. And easily gets me overly thrilled
-                    </p>
-                    <p className="bio">
-                    So far, I've learnt a little bit about <strong>Python, databases, JavaScript, React, and CSS</strong>, and I'm looking for an internship and, ideally, a permanent employment after that.
-                    </p>
-                    
-                </div>
-            </Animator>
+                <Animator animation={batch(Fade(), Sticky(), MoveOut(1000, 500))}>
+
+                    <div className="details">
+                        <h2 className="gretting">
+                            Hi there!
+                        </h2>
+                        <p className="bio">
+                            I’m a developer based in Helsinki, Finland, currently pursuing a Master’s in Computer Science at the University of Helsinki.
+                        </p>
+                        <p className="bio">
+                            I’m driven by curiosity, with an endless energy that fuels my interests, hobbies, and passion for learning.
+                        </p>
+                        <p className="bio">
+                            So far, I've explored <strong>Python, Kotlin, JavaScript, React, and PostgreSQL</strong>, and I’m looking for an internship with the potential to lead to a permanent role.
+                        </p>
+
+                    </div>
+                </Animator>
             </ScrollPage></>
     )
 }
